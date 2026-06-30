8a4d7e2b19c0052861d6fe6a03f6f8595d0c47a7
Ismail <Ismailchaudry123@icloud.com>
2026-06-30 06:38:51 -0500

    creates sequences

--- src/deeplearning.py
+++ src/deeplearning.py
@@ -69,3 +69,10 @@ def train_lstm(model, X_train, y_train, X_test, y_test, epochs=50, batch_size=32
     
     return model
     
+def create_sequences(data, seq_length=24):
+    X, y = [], []
+
+    for i in range(len(data)-seq_length):
+        X.append(data[i:i + seq_length])
+        y.append(data[i + seq_length])
+    return np.array(X), np.array(y)
