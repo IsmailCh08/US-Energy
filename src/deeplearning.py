efa88e06bc28dc37265c4025f7f3d59ec57f4a8c
Ismail <Ismailchaudry123@icloud.com>
2026-06-30 06:14:36 -0500

    forward func, and init defined

--- src/deeplearning.py
+++ src/deeplearning.py
@@ -11,7 +11,22 @@ from sklearn.preprocessing import StandardScaler
 class LTSMModel(nn.Module):
 
     def __init__(self, input_size=1, hidden_size=64, num_layers=2, output_size=1):
-        super(LTSMModel, self).__init__()
+        super(LTSMModel, self).__init__() # load parent class
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.dropout = nn.Dropout(0.2)
-        self.fc = nn.Linear(hidden_size, output_size)
+        self.dropout = nn.Dropout(0.2) # remove 20% of neurons
+        self.fc = nn.Linear(hidden_size, output_size) # hidden size --> output size
+    
+    def forward(self,x):
+        # pass the input though the LSTM
+        out, _ = self.lstm(x)
+
+        # take only the last output
+        out = out[:, -1, :]
+
+        # apply dropout
+        out = self.dropout(out)
+
+        # convert to pred
+        out = self.fc(out)
+
+        return out
