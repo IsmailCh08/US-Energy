8412196a700e8bb426b27b3fd91032fafc8d3f52
Ismail <Ismailchaudry123@icloud.com>
2026-06-30 05:49:25 -0500

    start LTSM class

--- src/deeplearning.py
+++ src/deeplearning.py
@@ -7,3 +7,11 @@ from sklearn.preprocessing import StandardScaler
 
 
 # LSTM Model Architecture
+
+class LTSMModel(nn.Module):
+
+    def __init__(self, input_size=1, hidden_size=64, num_layers=2, output_size=1):
+        super(LTSMModel, self).__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.dropout = nn.Dropout(0.2)
+        self.fc = nn.Linear(hidden_size, output_size)
